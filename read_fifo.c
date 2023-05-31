fc4a36905213285b34a1a5cfb22527b61424f599
tecnauRasp <duney.bsolo@gmail.com>
2023-05-31 15:08:52 +0200

    create a child process

--- read_fifo.c
+++ read_fifo.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <errno.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "adxl357.h"
 
 #define CE_ADXL 0
@@ -11,7 +13,15 @@
 float OutputDataRate = 4000;        // 4KHz
 float SamplingInterval = 0.00025;   // 1/4000 = 250ms
 
-
+/**
+ * Idea of function that can clear a little bit the code.
+ * There is a major problem in doing so, code is clearer but
+ * i have to put all the correct variables in the correct function.
+ * 
+ * If there are some variables that comunates from function to function
+ * i'm is a little complicaed, but it can be fixed using a pipe
+ * so you can comunicate from process to process.
+*/
 void write_in_file();
 void reading_from_sensor();
 void empty_fifo();
@@ -54,139 +64,157 @@ int main(int argc, char *argv[]) {
   int ciclo = 0;
   clock_t begin = clock();
 
-  while (1) {  
-    long rawX = 0, rawY = 0, rawZ = 0;
-    unsigned char isEmptyX = 0, isEmptyY = 0, isEmptyZ = 0; 
-    
-
-    // get status
-    int status = Adxl357_GetStatus(spi0);
-    int dataReady = (status & ADXL357_REG_STATUS_BIT_DATA_RDY) ? 1 : 0;
-    int fifoOvr = (status & ADXL357_REG_STATUS_BIT_FIFO_OVR) ? 1 : 0;
-    int fifoFull = (status & ADXL357_REG_STATUS_BIT_FIFO_FULL) ? 1 : 0;
-    unsigned char fifoEntries = Adxl357_GetFifoEntries(spi0);
-
-    // fprintf(logFile, "Status=%d - DataReady=%d - FifoFull=%d - FifoOvr=%d.- FifoEntries=%d\n", status, dataReady, fifoFull, fifoOvr, fifoEntries);
-    // printf("Status=%d - DataReady=%d - FifoFull=%d - FifoOvr=%d- FifoEntries=%d\n", status, dataReady, fifoFull, fifoOvr, fifoEntries);
-    // printf("Ciclo=%d - Entries=%d\n", ciclo, fifoEntries);
-
-    // clock_gettime(CLOCK_MONOTONIC, &starttime);
-
-    if (fifoOvr) {
-      fprintf(logFile, "FIFO has overrun.\n");
-      printf("FIFO has overrun.\n");
-      return 0;
-
-
-    }
-
-    while (fifoEntries > 0) {
-      
+  
+  if(fork() == 0) {
+  
+    while (1) {  
+      long rawX = 0, rawY = 0, rawZ = 0;
+      unsigned char isEmptyX = 0, isEmptyY = 0, isEmptyZ = 0; 
       
 
-      if (isFifoAligned == 0) {
-        // read data
-        Adxl357_GetRawAccelFromFifo(spi0, &rawX, &isEmptyX, &isX);
-        fifoEntries--;
+      // get status
+      int status = Adxl357_GetStatus(spi0);
+      int dataReady = (status & ADXL357_REG_STATUS_BIT_DATA_RDY) ? 1 : 0;
+      int fifoOvr = (status & ADXL357_REG_STATUS_BIT_FIFO_OVR) ? 1 : 0;
+      int fifoFull = (status & ADXL357_REG_STATUS_BIT_FIFO_FULL) ? 1 : 0;
+      unsigned char fifoEntries = Adxl357_GetFifoEntries(spi0);
 
-        if (isEmptyX == 0 && isX == 1) {
-          isFifoAligned = 1;
-          accDataRead = 1;
-          fprintf(logFile, "X detected (FifoEntries=%d).\n", (fifoEntries + 1));
-          // printf("X detected (FifoEntries=%d).\n", (fifoEntries + 1));
+      // fprintf(logFile, "Status=%d - DataReady=%d - FifoFull=%d - FifoOvr=%d.- FifoEntries=%d\n", status, dataReady, fifoFull, fifoOvr, fifoEntries);
+      // printf("Status=%d - DataReady=%d - FifoFull=%d - FifoOvr=%d- FifoEntries=%d\n", status, dataReady, fifoFull, fifoOvr, fifoEntries);
+      // printf("Ciclo=%d - Entries=%d\n", ciclo, fifoEntries);
 
-          //printf("X=%d(%d)", rawX, (fifoEntries + 1));
-        }
+      // clock_gettime(CLOCK_MONOTONIC, &starttime);
 
-
-      } else {
-        
-        // read data
-        if (accDataRead == 0) {
-          Adxl357_GetRawAccelFromFifo(spi0, &rawX, &isEmptyX, &isX);
-          // printf("X=%d(%d-%d-%d)", rawX, fifoEntries, isEmptyX, isX);
-          fifoEntries--;
-          accDataRead++;
-
-          // check data
-          if (isEmptyX == 1 || isX == 0) {
-            isFifoAligned = 0;
-            accDataRead = 0;
-            fprintf(logFile, "FIFO X data incorrect (isEmpty=%d / isX=%d).\n", isEmptyX, isX);
-            printf("FIFO X data incorrect (isEmpty=%d / isX=%d).\n", isEmptyX, isX);
-            return 0;
+      if (fifoOvr) {
+        fprintf(logFile, "FIFO has overrun.\n");
+        printf("FIFO has overrun.\n");
+        return 0;
 
 
-          }
-
-        } else if (accDataRead == 1) {
-          Adxl357_GetRawAccelFromFifo(spi0, &rawY, &isEmptyY, &isX);
-          // printf(" / Y=%d(%d-%d-%d)", rawY, fifoEntries, isEmptyY, isX);
-          fifoEntries--;
-          accDataRead++;
-
-          // check data
-          if (isEmptyY == 1 || isX == 1) {
-            isFifoAligned = 0;
-            accDataRead = 0;
-            fprintf(logFile, "FIFO Y data incorrect (isEmpty=%d / isX=%d).\n", isEmptyY, isX);
-            printf("FIFO Y data incorrect (isEmpty=%d / isX=%d).\n", isEmptyY, isX);
-            return 0;
-
-          
-          }
-
-        } else if (accDataRead == 2) {
-          Adxl357_GetRawAccelFromFifo(spi0, &rawZ, &isEmptyZ, &isX);
-          // printf(" / Z=%d(%d-%d-%d)\n", rawZ, fifoEntries, isEmptyZ, isX);
-          fifoEntries--;
-          accDataRead = 0;
-          
-          // check data
-          if (isEmptyZ == 1 || isX == 1) {
-            isFifoAligned = 0;
-            // accDataRead = 0;
-            fprintf(logFile, "FIFO Z data incorrect (isEmpty=%d / isX=%d).\n", isEmptyZ, isX);
-            printf("FIFO Z data incorrect (isEmpty=%d / isX=%d).\n", isEmptyZ, isX);
-            return 0;
-
-           
-          } else {
-            float accX = 0, accY = 0, accZ = 0;
-            // write in file
-            accX = Adxl357_ConvertAccelData(rawX, ADXL357_RANGE_40G);
-            accY = Adxl357_ConvertAccelData(rawY, ADXL357_RANGE_40G);
-            accZ = Adxl357_ConvertAccelData(rawZ, ADXL357_RANGE_40G);
-            fprintf(outFile, "%d; %f; %f;%f;%f;\n", sample, sampleTime, accX, accY, accZ);
-            //printf("Data converted: X=%f / Y=%f / Z=%f.\n", accX, accY, accZ);
-            sample++;
-            sampleTime += SamplingInterval;
-
-
-            //Open another file after 30 sec
-            long time_passed = (clock() - begin) / CLOCKS_PER_SEC;
-            if (time_passed >= 30){
-              begin = clock();
-              fclose(outFile);
-              printf("time passed = %d\n", time_passed);
-              sprintf(name, "%s%d.csv", fileName, ++number_of_out_files);
-              outFile = fopen(name, "w+");
-            }
-
-
-          }
-        }
       }
 
-    }
+      while (fifoEntries > 0) {
+        
+        
+
+        if (isFifoAligned == 0) {
+          // read data
+          Adxl357_GetRawAccelFromFifo(spi0, &rawX, &isEmptyX, &isX);
+          fifoEntries--;
+
+          if (isEmptyX == 0 && isX == 1) {
+            isFifoAligned = 1;
+            accDataRead = 1;
+            fprintf(logFile, "X detected (FifoEntries=%d).\n", (fifoEntries + 1));
+            // printf("X detected (FifoEntries=%d).\n", (fifoEntries + 1));
+
+            //printf("X=%d(%d)", rawX, (fifoEntries + 1));
+          }
+
+
+        } else {
+          
+          // read data
+          if (accDataRead == 0) {
+            Adxl357_GetRawAccelFromFifo(spi0, &rawX, &isEmptyX, &isX);
+            // printf("X=%d(%d-%d-%d)", rawX, fifoEntries, isEmptyX, isX);
+            fifoEntries--;
+            accDataRead++;
+
+            // check data
+            if (isEmptyX == 1 || isX == 0) {
+              isFifoAligned = 0;
+              accDataRead = 0;
+              fprintf(logFile, "FIFO X data incorrect (isEmpty=%d / isX=%d).\n", isEmptyX, isX);
+              printf("FIFO X data incorrect (isEmpty=%d / isX=%d).\n", isEmptyX, isX);
+              return 0;
+
+
+            }
+
+          } else if (accDataRead == 1) {
+            Adxl357_GetRawAccelFromFifo(spi0, &rawY, &isEmptyY, &isX);
+            // printf(" / Y=%d(%d-%d-%d)", rawY, fifoEntries, isEmptyY, isX);
+            fifoEntries--;
+            accDataRead++;
+
+            // check data
+            if (isEmptyY == 1 || isX == 1) {
+              isFifoAligned = 0;
+              accDataRead = 0;
+              fprintf(logFile, "FIFO Y data incorrect (isEmpty=%d / isX=%d).\n", isEmptyY, isX);
+              printf("FIFO Y data incorrect (isEmpty=%d / isX=%d).\n", isEmptyY, isX);
+              return 0;
+
+            
+            }
+
+          } else if (accDataRead == 2) {
+            Adxl357_GetRawAccelFromFifo(spi0, &rawZ, &isEmptyZ, &isX);
+            // printf(" / Z=%d(%d-%d-%d)\n", rawZ, fifoEntries, isEmptyZ, isX);
+            fifoEntries--;
+            accDataRead = 0;
+            
+            // check data
+            if (isEmptyZ == 1 || isX == 1) {
+              isFifoAligned = 0;
+              // accDataRead = 0;
+              fprintf(logFile, "FIFO Z data incorrect (isEmpty=%d / isX=%d).\n", isEmptyZ, isX);
+              printf("FIFO Z data incorrect (isEmpty=%d / isX=%d).\n", isEmptyZ, isX);
+              return 0;
+
+            
+            } else {
+              float accX = 0, accY = 0, accZ = 0;
+              // write in file
+              accX = Adxl357_ConvertAccelData(rawX, ADXL357_RANGE_40G);
+              accY = Adxl357_ConvertAccelData(rawY, ADXL357_RANGE_40G);
+              accZ = Adxl357_ConvertAccelData(rawZ, ADXL357_RANGE_40G);
+              fprintf(outFile, "%d; %f; %f;%f;%f;\n", sample, sampleTime, accX, accY, accZ);
+              //printf("Data converted: X=%f / Y=%f / Z=%f.\n", accX, accY, accZ);
+              sample++;
+              sampleTime += SamplingInterval;
+
+
+              //Open another file after 30 sec
+              long time_passed = (clock() - begin) / CLOCKS_PER_SEC;
+              if (time_passed >= 30){
+                begin = clock();
+                fclose(outFile);
+                printf("time passed = %d\n", time_passed);
+                sprintf(name, "%s%d.csv", fileName, ++number_of_out_files);
+                outFile = fopen(name, "w+");
+              }
+
+
+            }
+          }
+        }
+
+      }
 
     ciclo++;
 
     // clock_gettime(CLOCK_MONOTONIC, &stoptime);
     // double time_spent = ((stoptime.tv_sec - starttime.tv_sec)*1e9 + (stoptime.tv_nsec - starttime.tv_nsec)) / 1e6;
     // fprintf(errFile, "%d - Time spent = %d.\n", cycle, time_spent);
-  }
+    }
+
+    exit(0);
   
+  } else {
+
+    printf("hello i'm the father\n");
+
+    wait(NULL);
+
+    printf("YOU KILL MY CHILD!!!\n");
+    
+  }
+
+
+
+
   fclose(outFile);
   fclose(logFile);
   
